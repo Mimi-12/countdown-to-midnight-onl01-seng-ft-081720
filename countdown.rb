a7e0506498b714dd1ce9702c2de84b6c9443e2ce
selam lemma <oneiconforlife26@gmail.com>
2020-08-25 12:25:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
 
-def countdown
+def countdown(number)
+  while number <= 10
+  puts "#{number} SECOND(S)!"
+
+end
+   "HAPPY NEW YEAR!"
+  end
+
+
+def countdown_with_sleep
+  while condition == 0
+  end
 end
